c7fc3667145d2efa10066fac1878a4ce31bd0b27
indrajeetPanjiyar <indrajeetkr625@gmail.com>
2025-11-06 00:37:10 +0530

    Fourth Commit

--- frontend/src/components/VehiclePanel.jsx
+++ frontend/src/components/VehiclePanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VehiclePanel = (props) => {
     return (
-        <div>
+        <div className="max-h-[90vh] overflow-y-auto px-4 pb-6 pt-10 relative bg-white rounded-t-2xl">
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
                 props.setVehiclePanel(false)
             }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
